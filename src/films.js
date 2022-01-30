b42aea3c3e3160cb0115480b731bb96bae08934a
cobet <cobete80@gmail.com>
2022-01-30 18:22:10 +0100

    exercise 4

--- src/films.js
+++ src/films.js
@@ -14,20 +14,22 @@ function getMoviesFromDirector(movies, director) {
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(movies, director) {
-  let moviesDirector = getMoviesFromDirector(movies, director)
-  let moviesAverage = moviesDirector.reduce((counter,{score}) =>(counter + score) /2 ,moviesDirector[0].score)
+  let moviesDirector = getMoviesFromDirector(movies, director) // tengo que igualar la variable con la funcion getMoviesFromDirector del ejercicio 2 porque pide las peliculas de un determinado director
+  let moviesAverage = moviesDirector.reduce((counter,{score}) =>(counter + score) /2 ,moviesDirector[0].score)//en vez usar movies, debo usar moviesDirector
 
 return moviesAverage;
 }
 
 // Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(movies) {
-  let titleMovies = movies.map((movie) => movie.title); //ordenamos por titulo
-  let titlesMoviesOrder = titleMovies.sort ((a, b) => { //ordenamos alfabeticamente
-    if (a.title > b.title) {return 1;}
-    if (a.title < b.title) {return -1;}
+  let titleMovies = movies.map((movie) => movie.title); //ordenamos las peliculas por titulo
+  let titlesMoviesOrder = titleMovies.sort ((titleA, titleB) => { //ordenamos alfabeticamente
+    if (titleA < titleB) {return -1;}
+    if (titleA > titleB) {return 1;}
     return 0;
 });
+  let top20movies = titlesMoviesOrder.slice (0,20);
+  return top20movies;
 }
 
 // Exercise 5: Order by year, ascending
